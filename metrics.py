95779239239c10a9015ee0ebc98f260e0186c1a3
Anastasia Shvydkaiia <166436183+AnastasiaShvydkaiia@users.noreply.github.com>
2026-04-17 23:42:29 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -6,10 +6,9 @@ class MetricsTracker:
         self.g_mean = metrics.GeometricMean()
         
         self.current_step = 0
-        #self.detection_window = detection_window
 
         self.stable_steps = 0
-        self.false_alarms = 0 # Количество событий ложных срабатываний
+        self.false_alarms = 0 
 
         self.injected_drifts = [] 
         self.detected_drift_ids = set() 
@@ -23,7 +22,6 @@ class MetricsTracker:
         if manual_drifts is not None:
             self.injected_drifts = manual_drifts
 
-        # Drift Detections
         if drift_detected:
             # find closest true drift not yet matched
             closest_idx = None
@@ -32,7 +30,7 @@ class MetricsTracker:
                 if i in self.detected_drift_ids:
                     continue
                 dist = self.current_step - drift_step
-                # allow only forward matching (after drift happens)
+                # only forward matching (after drift happens)
                 if dist >= 0 and dist < closest_dist:
                     closest_dist = dist
                     closest_idx = i
@@ -41,19 +39,16 @@ class MetricsTracker:
                 self.detected_drift_ids.add(closest_idx)
                 delay = closest_dist
                 self.all_delays.append(delay)
-            # False Alarms
+            # false alarms
             else:
                 self.false_alarms += 1
 
         self.current_step += 1
 
     def calculate_hdd(self):
-        # Ad (Recall/Sensitivity) - доля пойманных дрейфов
         total_inj = len(self.injected_drifts)
         ad = len(self.detected_drift_ids) / total_inj if total_inj > 0 else 1.0
         
-        # A0 (Reliability) - доверие к сигналу (событийный подход)
-        # Насколько часто сигнал детектора оказывается истинным
         real_detects = len(self.detected_drift_ids)
         total_signals = real_detects + self.false_alarms
         a0 = real_detects / total_signals if total_signals > 0 else 1.0
